5bfc318c151d517e808e21f7eb06db528bbc1f0d
Mugeha <mugehajacky@gmail.com>
2025-08-09 14:15:08 +0300

    Setup frontend with React and global CSS styles

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Online Learning Platform</h1>
+      <p>Welcome to our platform!</p>
+    </div>
+  );
+}
+
+export default App;
